4a089a7617d4da6ee64646ef75490764adbcd065
宗云峰 <zongyunfeng@zongyunfengdeiMac.local>
2020-08-05 16:14:52 +0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	trailingComma: 'es5',
+	tabWidth: 4,
+	semi: false,
+	singleQuote: true,
+	arrowParens: 'avoid',
+	bracketSpacing: true,
+	useTabs: true,
+	jsxBracketSameLine: true,
+	quoteProps: 'as-needed',
+}
--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let values = []
+let currentHook = 0
+
+const useState = initialState => {
+	if (typeof values[currentHook] === 'undefined') {
+		values[currentHook] = initialState
+	}
+
+	let hookIndex = currentHook
+	const setState = nextValue => {
+		values[hookIndex] = nextValue
+		ReactDOM.render(<MyName />, document.getElementById('root'))
+	}
+
+	return [values[currentHook++], setState]
 }
 
-export default App;
+const MyName = () => {
+	currentHook = 0
+
+	const [enableFirstName, setEnableFirstName] = useState(false)
+	const [name, setName] = useState('')
+	const [lastName, setLastName] = useState('')
+
+	const handleChange = evt => {
+		setName(evt.target.value)
+	}
+
+	const handleLastNameChange = evt => {
+		setLastName(evt.target.value)
+	}
+
+	const handleEnableChange = () => {
+		setEnableFirstName(!enableFirstName)
+	}
+
+	return (
+		<div
+			style={{
+				display: 'flex',
+				flex: 1,
+				flexDirection: 'column',
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}>
+			<div>
+				<h1>
+					My name is: {enableFirstName ? name : ''} {lastName}
+				</h1>
+				<input
+					type="checkbox"
+					value={enableFirstName}
+					onChange={handleEnableChange}
+				/>
+				<input type="text" value={name} onChange={handleChange} />
+				<input
+					type="text"
+					value={lastName}
+					onChange={handleLastNameChange}
+				/>
+			</div>
+		</div>
+	)
+}
+
+export default MyName
